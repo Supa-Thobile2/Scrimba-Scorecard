f8dc7bec5cb9e1ed36692bc74c1753c7af56ca03
Supa-Thobile2 <thobilenontenja@gmail.com>
2022-08-12 15:50:57 +0200

    updated project

---
+++ index.js
@@ -0,0 +1,12 @@
+let score = document.getElementById("score");
+let scoreCount = 0;
+
+function increase(){
+  
+    // scoreCount = scoreCount + 1;
+    // score.innerText = scoreCount;
+    console.log("button clicked")
+
+}
+
+increase()
