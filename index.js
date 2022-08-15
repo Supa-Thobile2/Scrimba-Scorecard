fc1c7879583d224341e1dfc0f76ac96a1bdc9d80
Supa-Thobile2 <thobilenontenja@gmail.com>
2022-08-15 09:07:20 +0200

    updated project

--- index.js
+++ index.js
@@ -1,12 +1,54 @@
-let score = document.getElementById("score");
-let scoreCount = 0;
+let homeScore = document.getElementById("homeScore");
+let guestScore = document.getElementById("guestScore");
+let homeScoreCount = 0;
+let guestScoreCount = 0;
 
 function increase(){
   
-    // scoreCount = scoreCount + 1;
-    // score.innerText = scoreCount;
-    console.log("button clicked")
+    homeScoreCount =  homeScoreCount + 1;
+    homeScore.innerText =  homeScoreCount;
+    // console.log("button clicked")
 
 }
 
-increase()
+function increaseByTwo(){
+  
+    homeScoreCount =  homeScoreCount + 2;
+    homeScore.innerText =  homeScoreCount;
+    // console.log("button clicked")
+
+}
+
+function increaseByThree(){
+  
+    homeScoreCount =  homeScoreCount + 3;
+    homeScore.innerText =  homeScoreCount;
+    // console.log("button clicked")
+
+}
+
+// increase()
+
+function increaseHome(){
+  
+    guestScoreCount =   guestScoreCount  + 1;
+    guestScore.innerText = guestScoreCount;
+    // console.log("button clicked")
+
+}
+
+function increaseByTwoHome(){
+  
+    guestScoreCount =   guestScoreCount  + 2;
+    guestScore.innerText = guestScoreCount;
+    // console.log("button clicked")
+
+}
+
+function increaseByThreeHome(){
+  
+    guestScoreCount =   guestScoreCount  + 3;
+    guestScore.innerText = guestScoreCount;
+    // console.log("button clicked")
+
+}
